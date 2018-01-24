77e13f501048f0d09b0da28a84aeceaa2feae24e
Philip Whitfield <philip@undef.ch>
2018-01-24 21:44:52 +0100

    server parse command line params

--- server/server.cpp
+++ server/server.cpp
@@ -15,9 +15,10 @@ int main(int argc, char** argv) {
 	std::string host = "127.0.0.1";
 	auto port = OpenNI2ServerDefaultPort;
 
-	if(argc == 3){
+	if(argc > 2){
 		host = argv[1];
-		port = std::atoi(argv[2]);
+		if(argc > 3)
+			port = std::atoi(argv[2]);
 	}else{
 		LOGI << "optional usage ./openni2-net-stream-server host port";
 	}
